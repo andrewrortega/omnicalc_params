a68772a43c39724b0028a5e36a1c3d3343a95a92
Andrew Ortega <andrewrortega@chicagobooth.edu>
2017-04-25 18:07:15 -0500

    update 2
    flex payment and rand number included

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -1,11 +1,45 @@
 class CalculationsController < ApplicationController
+  # flexible square
   def flex_square_5
     @user_number = params["num"].to_f
     @square = @user_number**2
     render("calculations/flex_square_5.html.erb")
   end
-
+  # flexible square root
   def flex_square_root_5
+    @user_number = params["num"].to_f
+    @square_root = @user_number**0.5
+    @square_root_final = @square_root.round(2)
+    render("calculations/flex_square_root_5.html.erb")
+  end
+  # flexible payment
+  def flex_payment_num
+    @interest_rate = params["num_1"].to_f / 10000 / 12
+    @term = params["num_2"].to_f * -1 * 12
+    @principal = params["num_3"].to_f
+    @monthly_payment_numerator = @principal * @interest_rate
+    @monthly_payment_denominator = 1 - (1 + @interest_rate)**@term
+    @monthly_payment = @monthly_payment_numerator / @monthly_payment_denominator
+    @monthly_payment_final = @monthly_payment.round(2)
+    render("calculations/flex_payment_num.html.erb")
+  end
+  # random number
+  def flex_random_num
+    @low_num = params["num_1"].to_f.round(0)
+    @high_num = params["num_2"].to_f.round(0)
+    @random_number = rand(@low_num..@high_num)
+    @random_number_final = @random_number.round(0)
+    render("calculations/random_num.html.erb")
+  end
+
+
+
+
+
+
+
+
+  def flex_square_root_num
     @user_number = params["num"].to_f
     @square_root = @user_number**0.5
     render("calculations/flex_square_root_5.html.erb")
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,20 @@
 Rails.application.routes.draw do
-
+  # flexiblw square
   get("/flexible/square/:num", {:controller => "calculations", :action => "flex_square_5"})
+
+  # flexible square root
   get("/flexible/square_root/:num", {:controller => "calculations", :action => "flex_square_root_5"})
+
+  # flexible payment
+  get("/flexible/payment/:num_1/:num_2/:num_3", {:controller => "calculations", :action => "flex_payment_num"})
+  # flexible random number
+  get("/flexible/random/:num_1/:num_2", {:controller => "calculations", :action => "flex_random_num"})
+
+
+
+
+
+
   get("/square/new", {:controller => "calculations", :action => "square_form"})
   get("/square/results", {:controller => "calculations", :action => "square"})
 end
